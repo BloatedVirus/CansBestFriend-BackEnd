8a4ecfcea304ddc4998c9328b7376b084c03cabe
BloatedVirus <darkoboesmans@gmail.com>
2021-07-28 14:37:01 +0200

    alles toevoegen

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6c5b6127cc94f486fae858edfdb95711'),
+    },
+  },
+});
